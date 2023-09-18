b693ff86f9ba8d908dfaf1823eec80dd06b3ad79
Faran71 <muhammadfaran01.mfs@gmail.com>
2023-09-18 18:30:48 +0100

    added flip cards to main page

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./MainPage.css"
+import mountain_image from "./mountain_image.jpg"
 
 const MainPage = () => {
     const navigate = useNavigate();
@@ -9,12 +10,46 @@ const MainPage = () => {
         return(
             <div className="main-page">
                 <header>
-                    <button onClick={() => navigate("/Jokes")}>Jokes</button>
-                    <button onClick={() => navigate("/Fact")}>Fact</button>
-                    <button onClick={() => navigate("/Quote")}>Quote</button>
-                    <button onClick={() => navigate("/AgeGuess")}>Age Guesser</button>
+                    <button onClick={() => navigate("/Jokes")} className="btn">Jokes</button>
+                    <button onClick={() => navigate("/Fact")} className="btn">Fact</button>
+                    <button onClick={() => navigate("/Quote")} className="btn">Quote</button>
+                    <button onClick={() => navigate("/AgeGuess")} className="btn">Age Guesser</button>
                 </header>
-                <h2>Main Page</h2>
+                <div className="collection">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={mountain_image} alt="Avatar"  />
+                            </div>
+                            <div class="flip-card-back">
+                                <h1>A</h1>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={mountain_image} alt="Avatar"  />
+                            </div>
+                            <div class="flip-card-back">
+                                <h1>B</h1>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src={mountain_image} alt="Avatar"  />
+                            </div>
+                            <div class="flip-card-back">
+                                <h1>C</h1>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     
